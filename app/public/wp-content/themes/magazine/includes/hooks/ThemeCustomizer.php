93bacfc315bacce88c1669e8cf36351ebb3302e8
Thunay Moreira de Soares <thunaymoreiradesoares@gmail.com.br>
2024-03-21 13:42:11 -0300

    🚧 Header

--- app/public/wp-content/themes/magazine/includes/hooks/ThemeCustomizer.php
+++ app/public/wp-content/themes/magazine/includes/hooks/ThemeCustomizer.php
@@ -9,6 +9,7 @@ const CUSTOM_LOGO_SIZE = [
     'width'       => 220,
     'height'      => 186,
 ];
+const CUSTOM_COLORS = ['page_color'];
 
 class ThemeCustomizer
 {
@@ -16,6 +17,9 @@ class ThemeCustomizer
     {
         add_action('after_setup_theme', [$this, 'custom_logo']);
         add_action('graphql_register_types', [$this, 'custom_logo_graphql']);
+
+        add_action('customize_register',     [$this, 'custom_colors']);
+        add_action('graphql_register_types', [$this, 'custom_colors_graphql']);
     }
 
     public function custom_logo($wp_customize) {
@@ -49,6 +53,52 @@ class ThemeCustomizer
                     'width'       => CUSTOM_LOGO_SIZE['width'],
                     'height'      => CUSTOM_LOGO_SIZE['height'],
                 ];
+            },
+        ]);
+    }
+
+    public function custom_colors($wp_customize) {
+        $wp_customize->add_section('custom_colors', [
+            'title' => esc_html__('Cores', 'magazine'),
+        ]);
+
+        $wp_customize->add_setting('page_color', [
+            'default' => '#FFFFFF',
+        ]);
+
+        $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'page_color', [
+            'label'    => esc_html__('Página', 'magazine'),
+            'section'  => 'custom_colors',
+            'settings' => 'page_color',
+            'priority' => 1,
+        ]));
+    }
+
+    public function custom_colors_graphql() {
+        $custom_colors_object_fields = [];
+
+        foreach(CUSTOM_COLORS as $color) {
+            $custom_colors_object_fields[$color] = ['type' => 'String'];
+        }
+
+        register_graphql_object_type('CustomColors', [
+            'description' => __('Cores personalizadas do Customizer', 'magazine'),
+            'fields' => [
+                ...$custom_colors_object_fields
+            ],
+        ]);
+
+        register_graphql_field( 'RootQuery', 'CustomColors', [
+            'type' => 'CustomColors',
+            'resolve' => function () {
+
+                $custom_color_fields = [];
+
+                foreach(CUSTOM_COLORS as $color) {
+                    $custom_color_fields[$color] = get_theme_mod($color) ? get_theme_mod($color) : '#FFFFFF';
+                }
+
+                return $custom_color_fields;
             },
         ]);
     }
